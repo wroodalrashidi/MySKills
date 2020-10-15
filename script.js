b84674c612d66bbae550f7b7edcb36bbcaa4a124
ورود الرشيدي <66529914+wroodalrashidi@users.noreply.github.com>
2020-10-16 01:37:14 +0300

    working on it

---
+++ script.js
@@ -0,0 +1,18 @@
+function profiles() {
+
+english-teacher{
+    name: "noor";
+    degree: "colleage degree";
+    expericence: "10 years";
+}
+
+{
+    name: "mark";
+    degree: "colleage degree";
+    expericence: "10 years";
+}
+
+}
+
+
+
