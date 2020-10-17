dc635c1fc4f54c864e6baf292440633fb1a6057a
ورود الرشيدي <66529914+wroodalrashidi@users.noreply.github.com>
2020-10-18 01:53:43 +0300

    ...

--- resume.js
+++ resume.js
@@ -1,14 +1,40 @@
-function sendEmail() {
-    Email.send({
-        Host : "smtp.mailtrap.io",
-        Username : "<Mailtrap username>",
-        Password : "<Mailtrap password>",
-        To : 'recipient@example.com',
-        From : "sender@example.com",
-        Subject : "Test email",
-        Body : "<html><h2>Header</h2><strong>Bold text</strong><br></br><em>Italic</em></html>"
-    }).then(
-      message => alert(message)
-    );
-    }
+var tutoringResumes = [
+  {
+      profile: '<img style="width: 100px; height: 120px; border-radius: 5px;" src="./profile-pics/english-teacher.jpg" >',
+      name: "Ms. Noor",
+      degree: "Bachelor",
+      experience: "Teaches high school students"
+  },
 
+  {
+      profile: '<img style="width: 100px; height: 120px; border-radius: 5px;" src="./profile-pics/math-teacher.png" >',
+      name: "Ms. Mike",
+      degree: "Bachelor",
+      experience: "taught for 10 years"
+  },
+
+  {
+      profile: '<img style="width: 100px; height: 120px; border-radius: 5px;" src="./profile-pics/ physics-teacher.jpg" >',
+      name: "Ms. Henry",
+      degree: "Bachelor",
+      experience: "5 years of teaching high school students"
+  }
+]
+
+
+tutoringResumes.forEach(tutor => {
+  var div = document.querySelector('div')
+
+  div.innerHTML = div.innerHTML + `
+        <div class = "menu" >
+          <span class = "divimg">
+          ${tutor.profile}
+          </span>
+          <br>   Name:  ${tutor.name } <br> Degree: ${tutor.degree} <br> Experience:  ${tutor.experience } <br>
+        </span>
+          <span>
+          </div>
+
+      
+`
+})
