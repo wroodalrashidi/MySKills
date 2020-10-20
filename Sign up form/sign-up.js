f548d7db2ab2eeebddd98cd25c357517475016f5
ورود الرشيدي <66529914+wroodalrashidi@users.noreply.github.com>
2020-10-20 07:24:30 +0300

    ...

--- Sign up form/sign-up.js
+++ Sign up form/sign-up.js
@@ -28,23 +28,12 @@ function validateForm() {
 
 }
 
+function password() {
+    var x = document.getElementById("pass-input");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
 
-// $("#ajaxForm").submit(function(e){
-//     e.preventDefault();
-//     var action = $(this).attr("action");
-//     var data = {};
-//     $(this).serializeArray().map(function(x){data[x.name] = x.value;}); 
-//     $.ajax({
-//       type: "POST",
-//       url: action,
-//       data: JSON.stringify(data),
-//       contentType: "application/json",
-//       headers: {
-//         "Accept": "application/json"
-//       }
-//     }).done(function() {
-//        $('.success').addClass('is-active');
-//     }).fail(function() {
-//        alert('An error occurred please try again later.')
-//     });
-//   });
