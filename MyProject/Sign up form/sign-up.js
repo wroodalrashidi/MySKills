b60ff803357397b72a2fc3b4cd326cbf29f7db31
ورود الرشيدي <66529914+wroodalrashidi@users.noreply.github.com>
2020-10-22 23:48:59 +0300

    ...

--- MyProject/Sign up form/sign-up.js
+++ MyProject/Sign up form/sign-up.js
@@ -6,27 +6,27 @@ function validateForm() {
 
    
    
-    if (name == "") {
-      alert("First Name must be filled out");
-      return false;
+//     if (name == "") {
+//       alert("First Name must be filled out");
+//       return false;
     
-    } else if (last == "") {
-        alert("Last Name must be filled out");
-        return false;
+//     } else if (last == "") {
+//         alert("Last Name must be filled out");
+//         return false;
     
-    } else if (email == "") {
-        alert("Email must be filled out");
-        return false;
+//     } else if (email == "") {
+//         alert("Email must be filled out");
+//         return false;
     
-    } else if (password == "") {
-        alert("Password must be filled out");
-        return false;
+//     } else if (password == "") {
+//         alert("Password must be filled out");
+//         return false;
     
-    } else{
-        alert("Error");  
-    }
+//     } else{
+//         alert("Error");  
+//     }
 
-}
+// }
 
 function password() {
     var x = document.getElementById("pass-input");
